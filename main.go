5ff6ef60ebc6fd30030da2aa0905b22925728608
Eric Caleb <caleberic@tuta.io>
2022-10-10 02:33:18 +0200

    add readyz route

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/iamcaleberic/semi-task/controllers"
+	"github.com/iamcaleberic/semi-task/models"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -19,9 +20,14 @@ func main() {
 		c.JSON(http.StatusOK, "Movie Gallery API")
 	})
 
+	r.GET("/readyz", func(c *gin.Context) {
+		models.Ping()
+		c.JSON(http.StatusOK, gin.H{"status": "OK"})
+	})
+
 	r.GET("/movies", controllers.GetMovies)
-	r.POST("/movies", controllers.CreateMovie)
 	r.GET("/movies/:id", controllers.GetMovie)
+	r.POST("/movies", controllers.CreateMovie)
 
 	// listen and serve on 0.0.0.0:$PORT
 	r.Run()
